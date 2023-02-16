61d842d6987b32135749bd8533bead7fdfc0a58a
Pedro Castaneda <petecast76@gmail.com>
2023-02-16 02:13:41 -0800

    Spruced things up

--- scripts/app.js
+++ scripts/app.js
@@ -2,21 +2,12 @@
 // 02-15-2023
 // In this assignment we are to build a random name generator application for desktop. The user will be able to split the list into groups, add new names to the list, remove names from the list, be able to save the list into local storage.
 
-// set up default list
-// be able to get input from user and add name to list
-// be able to display random name
-// be able to delete specific person from list
-// be able to set number of groups and number of people per group
-// create elements
-// test everything
-
 // Imports
 import { GetNames, SaveToLocalStorage, RemoveFromLocalStorage } from './localStorage.js'
-import { MakeCards } from './functions.js';
+import { MakeCards, MakeListItemForEntry, MakeListItemsForDropdown, shuffleArray } from './functions.js';
+
 // Global Variables
-// let namesList = GetNames();
-// console.log(namesList);
-let defaultData, studentData, isPopulated, randomIndex;
+let defaultData, studentData, randomIndex;
 let nameInput = document.getElementById('nameInput');
 let addNameBtn = document.getElementById('addNameBtn');
 let randomBtn = document.querySelector('#randomBtn');
@@ -27,7 +18,7 @@ let splitByGroup = document.querySelector('#splitByGroup');
 let displayName = document.querySelector('#displayName');
 let injectItems = document.querySelector('#injectItems');
 let injectGroups = document.querySelector('#injectGroups');
-// let fillList = document.querySelector('#fillList');
+let clearBtn = document.getElementById('clearBtn');
 
 // Functions
 // This function will get default names from json file
@@ -48,27 +39,23 @@ studentData = await GetStudentNames();
 // This function will populate list with default names
 const PopulateListWithDefaultNames = () => {
     let namesList = GetNames();
-    console.log('Names list in Populate function:');
-    console.log(namesList);
-    console.log('dataTest strinified!');
-    console.log(JSON.stringify(defaultData));
     defaultData.map(person => {
         if (!namesList.some(name => name === person.firstName)) {// condition checks to see if 
             SaveToLocalStorage(person.firstName);
-            console.log('Logged from populate func: ' + person.firstName);
         }
     });
 }
+
 // This function will  populate list with student names
 const PopulateListWithStudentNames = () => {
     let namesList = GetNames();
     studentData.map(person => {
         if (!namesList.some(name => name === person.firstName)) {
             SaveToLocalStorage(`${person.firstName} ${person.lastName}`);
-            console.log('Logged from student populate func: ' + person.firstName);
         }
     })
 }
+
 // This function checks to see if namesList is empty, if so then fill with test subjects
 const CheckNamesListForPopulation = () => {
     let namesList = GetNames();
@@ -78,90 +65,19 @@ const CheckNamesListForPopulation = () => {
     }
 }
 CheckNamesListForPopulation();
-// Creates list elements
-const MakeListItemsForDropdown = () => {
-    let namesList = GetNames();
-    console.log('Names list in MakeListItemsForDropdown:');
-    console.log(namesList);
-    let listEmptyTxt = document.createElement('p');
-    listEmptyTxt.textContent = 'List is currently empty, add names to the list.';
-    listEmptyTxt.className = 'text-center text-danger my-3';
-    if (namesList.length === 0) {
-        injectItems.append(listEmptyTxt);
-    }
-    else {
-        namesList.map(name => {
-            let deleteBtn = document.createElement('button');
-            deleteBtn.className = 'btn bg-danger';
-            deleteBtn.innerHTML = `<i class="fa-solid fa-delete-left"></i>`;
-            deleteBtn.id = 'deleteBtn';
-            let p = document.createElement('p');
-            p.textContent = name;
-            p.className = 'mx-3';
-            let nameContainer = document.createElement('div');
-            nameContainer.className = 'nameContainer border border-success bg-dark d-flex justify-content-between p-2 mb-1';
-            nameContainer.append(p, deleteBtn);
-            injectItems.append(nameContainer);
 
-            deleteBtn.addEventListener('click', function () {
-                RemoveFromLocalStorage(name);
-                injectItems.removeChild(nameContainer);
-                GetNames();
-            });
-        });
-    }
-}
-MakeListItemsForDropdown();
-// Creates list item when new name added
-const MakeListItemForEntry = (name) => {
-    let namesList = GetNames();
-    console.log('Names list in MakeListItemForEntry');
-    console.log(namesList);
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn bg-danger';
-    deleteBtn.innerHTML = `<i class="fa-solid fa-delete-left"></i>`;
-    deleteBtn.id = 'deleteBtn';
-    let p = document.createElement('p');
-    p.textContent = name;
-    p.className = 'mx-3';
-    let nameContainer = document.createElement('div');
-    nameContainer.className = 'nameContainer border border-success bg-dark d-flex justify-content-between p-2 mb-1';
-    nameContainer.append(p, deleteBtn);
-    injectItems.append(nameContainer);
-
-    deleteBtn.addEventListener('click', function () {
-        RemoveFromLocalStorage(name);
-        injectItems.removeChild(nameContainer);
-        GetNames();
-        console.log('Names list after delete btn pressed:')
-        console.log(namesList);
-    });
-}
-
+// This function will shuffle current names list, then use for loop to assing a new group array a slice of the shuffledArray. Then push group array to group(s) array. Repeat until end of shuffledArray. Finally, MakeCards for each group.
 const SplitByNumberOfPeople = (numOfPeople, namesArr) => {
-    let shuffledArray = shuffleArray(namesArr);
-    const numOfPplParsed = parseInt(numOfPeople);
-    const numOfGroups = Math.ceil(shuffledArray.length / numOfPplParsed);
-    let groups = [];
+    let shuffledArray = shuffleArray(namesArr);// shuffle using FY algorithm
+    const numOfPplParsed = parseInt(numOfPeople);// covert str to int
+    const numOfGroups = Math.ceil(shuffledArray.length / numOfPplParsed);// get num of groups
+    let groups = [];// intiate empty array for group arrays
     for (let i = 0; i < shuffledArray.length; i += numOfPplParsed) {
-        let group = [];
-        group = shuffledArray.slice(i, i + numOfPplParsed);
-        console.log('Check group:');
-        console.log(group);
-        groups.push(group);
-        console.log('Check group(s):')
-        console.log(groups);
+        let group = [];// initiate new array for every increment
+        group = shuffledArray.slice(i, i + numOfPplParsed);// slice next section of names list
+        groups.push(group);// 
     }
     MakeCards(groups, injectGroups);
-}
-
-// Fisher-Yates Algorithm - attribution: https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
 }
 
 // Event Listeners
@@ -170,15 +86,10 @@ addNameBtn.addEventListener('click', function () {
         alert('Please enter a name!');
     }
     let names = GetNames();// reinitialize list for updated list
-    console.log('Names in add btn');
-    console.log(names);
     let namesStr = JSON.stringify(names);// make names into strings
-    console.log('Names as strings in add btn');
-    console.log(namesStr);
     if (!namesStr.includes(JSON.stringify(nameInput.value))) {// check if name is in name array
-        console.log('Name input value in if statement in add btn: ' + nameInput.value);
         SaveToLocalStorage(nameInput.value);
-        MakeListItemForEntry(nameInput.value);
+        MakeListItemForEntry(nameInput.value, GetNames(), injectItems);
         nameInput.value = '';
         GetNames();
     }
@@ -188,7 +99,6 @@ randomBtn.addEventListener('click', function () {
     let namesList = GetNames();
     randomIndex = Math.floor(Math.random() * namesList.length);
     displayName.textContent = namesList[randomIndex];
-    console.log('Random btn clicked!');
 });
 
 splitByPeople.addEventListener('click', function () {
@@ -197,7 +107,7 @@ splitByPeople.addEventListener('click', function () {
     }
     else {
         // let namesList = 
-        console.log('input value for split by people: ', peopleInGroup.value, numberOfGroups.value);
+        injectGroups.innerHTML = '';
         SplitByNumberOfPeople(peopleInGroup.value, GetNames());
         peopleInGroup.value = '';
     }
@@ -206,3 +116,9 @@ splitByPeople.addEventListener('click', function () {
 splitByGroup.addEventListener('click', function () {
     numberOfGroups.value = '';
 });
+
+clearBtn.addEventListener('click', function(){
+    injectGroups.innerHTML = '';
+});
+
+MakeListItemsForDropdown(GetNames(), injectItems);
--- scripts/functions.js
+++ scripts/functions.js
@@ -1,34 +1,86 @@
+// Create card elements
 const MakeCards = (array, inject) => {
     console.log(array);
+    let count = 1;
     array.map(group => {
-        let count = 1;
         let ul = document.createElement('ul');
         ul.className = 'list-group list-group-flush';
         let cardHeader = document.createElement('div');
-        cardHeader.className = 'card-header'
+        cardHeader.className = 'card-header bg-dark'
         cardHeader.textContent = `Group ${count}`;
         let card = document.createElement('div');
-        card.className = 'card m-2';
+        card.className = 'card m-2 bg-dark';
         card.style = 'width: 18rem;';
         card.append(cardHeader, ul);
-        count++;
         group.map(person => {
             let li = document.createElement('li');
-            li.className = 'list-group-item';
+            li.className = 'list-group-item text-success bg-dark';
             li.textContent = person;
             ul.append(li);
         });
         inject.append(card);
+        count++;
+    });
+}
+// Creates list item when new name added
+const MakeListItemForEntry = (name, func, inject) => {
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn bg-danger';
+    deleteBtn.innerHTML = `<i class="fa-solid fa-delete-left"></i>`;
+    deleteBtn.id = 'deleteBtn';
+    let p = document.createElement('p');
+    p.textContent = name;
+    p.className = 'mx-3';
+    let nameContainer = document.createElement('div');
+    nameContainer.className = 'nameContainer border border-success bg-dark d-flex justify-content-between p-2 mb-1';
+    nameContainer.append(p, deleteBtn);
+    inject.append(nameContainer);
+
+    deleteBtn.addEventListener('click', function () {
+        RemoveFromLocalStorage(name);
+        inject.removeChild(nameContainer);
+        func;
     });
 }
 
-{/* <div class="card m-2" style="width: 18rem;">
-    <div class="card-header">
-        Featured
-    </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
-    </ul>
-</div> */}
+const MakeListItemsForDropdown = (func, inject) => {
+    let namesList = func;
+    let listEmptyTxt = document.createElement('p');
+    listEmptyTxt.textContent = 'List is currently empty, add names to the list.';
+    listEmptyTxt.className = 'text-center text-danger my-3';
+    if (namesList.length === 0) {
+        inject.append(listEmptyTxt);
+    }
+    else {
+        namesList.map(name => {
+            let deleteBtn = document.createElement('button');
+            deleteBtn.className = 'btn bg-danger';
+            deleteBtn.innerHTML = `<i class="fa-solid fa-delete-left"></i>`;
+            deleteBtn.id = 'deleteBtn';
+            let p = document.createElement('p');
+            p.textContent = name;
+            p.className = 'mx-3';
+            let nameContainer = document.createElement('div');
+            nameContainer.className = 'nameContainer border border-success bg-dark d-flex justify-content-between p-2 mb-1';
+            nameContainer.append(p, deleteBtn);
+            inject.append(nameContainer);
 
-export { MakeCards };
+            deleteBtn.addEventListener('click', function () {
+                RemoveFromLocalStorage(name);
+                inject.removeChild(nameContainer);
+                func;
+            });
+        });
+    }
+}
+
+// Fisher-Yates Algorithm - attribution: https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+export { MakeCards, MakeListItemForEntry, MakeListItemsForDropdown, shuffleArray };
